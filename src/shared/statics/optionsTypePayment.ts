5b557e5e5e44feac597dd55f1736857bc80e4df9
Matheus Reis <matheus.reis0806@gmail.com>
2026-02-14 18:59:11 -0300

    fix: update options payment

--- src/shared/statics/optionsTypePayment.ts
+++ src/shared/statics/optionsTypePayment.ts
@@ -1,8 +1,8 @@
 export const optionsTypePayment = [
-    { name: "Bradesco Matheus", value: "bradesco_matheus", dia_vencimento: 20 },
-    { name: "Nubank Matheus", value: "nubank_matheus", dia_vencimento: 10 },
-    { name: "Caixa Matheus", value: "caixa_matheus", dia_vencimento: 10 },
-    { name: "Bradesco Ana", value: "bradesco_ana", dia_vencimento: 10 },
-    { name: "Nubank Ana", value: "nubank_ana", dia_vencimento: 10 },
+    { name: "Bradesco Matheus", value: "bradesco_matheus", dia_vencimento: 5 },
+    { name: "Nubank Matheus", value: "nubank_matheus", dia_vencimento: 14 },
+    { name: "Caixa Matheus", value: "caixa_matheus", dia_vencimento: 18 },
+    { name: "Bradesco Ana", value: "bradesco_ana", dia_vencimento: 26 },
+    { name: "Nubank Ana", value: "nubank_ana", dia_vencimento: 16 },
     { name: "Pix", value: "pix" }
 ]
